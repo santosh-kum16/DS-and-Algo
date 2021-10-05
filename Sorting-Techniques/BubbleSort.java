8373b4276792fb4037099793c5ad6bfc4125ea79
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2021-10-05 12:34:02 +0530

    Code written

--- Sorting-Techniques/BubbleSort.java
+++ Sorting-Techniques/BubbleSort.java
@@ -1,1 +1,22 @@
-
+import java.util.Arrays;
+class BubbleSort 
+{
+	public static void main(String[] args) 
+	{
+		int data [] = {2,5,1,9,5,0,6};
+		int len = data.length;
+		for (int i=0; i<len ;++i )
+		{
+			for (int j=i+1; j<len ; ++j )
+			{
+				if (data[i] > data[j])
+				{
+					int temp = data[i];
+					data[i] = data[j];
+					data[j] = temp;
+				}
+			}
+		}
+		System.out.print("Sorted data: "+Arrays.toString(data));
+	}
+}
