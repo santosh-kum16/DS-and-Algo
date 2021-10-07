6b7af5bdd141815c3b9b7c25d0b3fabc22703f90
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2021-10-07 17:46:52 +0530

    Source Code Written

--- Sorting-Techniques/InsertionSort.java
+++ Sorting-Techniques/InsertionSort.java
@@ -1,1 +1,35 @@
+class InsertionSort {
+	void sort(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 1; i < n; ++i) {
+			int pointingVal = arr[i];
+			int comparingVal = i - 1;
+			while (comparingVal >= 0 && arr[comparingVal] > pointingVal) {
+				arr[comparingVal + 1] = arr[comparingVal];
+				comparingVal = comparingVal - 1;
+			}
+			arr[comparingVal + 1] = pointingVal;
+		}
+	}
+	static void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
 
+		System.out.println();
+	}
+	public static void main(String args[])
+	{
+		int arr[] = { 12, 11, 13, 5, 6 };
+
+		InsertionSort ob = new InsertionSort();
+		System.out.println("Before Sorting element in the Array are:");
+		ob.printArray(arr);
+		System.out.println("After Sorting element in the Array are:");
+		ob.sort(arr);
+
+		printArray(arr);
+	}
+}
