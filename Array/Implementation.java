71ecfde17355ee1a18c48ba76e0395eae346782f
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2021-10-09 23:46:04 +0530

    ways of implements array

--- Array/Implementation.java
+++ Array/Implementation.java
@@ -1,1 +1,18 @@
+//Below 3-ways we are Declaring ARRAY
+class AyyDeclaration
+{
+	public static void main(String[] args) 
+	{
+		//Array Declaration type 1
+		int ar[] = new int[5];
 
+		//Array Declaration type 2
+		int br[];
+		br=new int [5];
+		
+		//Array Declaration type 3
+		int cr[]={};
+		//All 3 array are created, So printing hash code of Object reference
+		System.out.println(ar+" "+br+" "+cr);
+	}
+}
