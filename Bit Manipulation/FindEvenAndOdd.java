aaa46d8fd24821d3169c4963b5c5e45cff3c3e96
Santosh Kumar <santosh.kum16@gmail.com>
2021-10-11 23:55:19 +0530

    Bit Manipulation

---
+++ Bit Manipulation/FindEvenAndOdd.java
@@ -0,0 +1,11 @@
+class FindEvenAndOdd 
+{
+	public static void main(String[] args) 
+	{
+		int num = 6;
+		if ((num & 1) == 0)
+			System.out.println("Number is EVEN");
+		else
+			System.out.println("Number is ODD");
+	}
+}
