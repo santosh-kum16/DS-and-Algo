02b7a95932aa9c350ece38f98d002d8176a0182e
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2021-10-13 23:59:57 +0530

    initial commit

---
+++ Array/ConvertArrayToString.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class ArrayToString 
+{
+	public static void main(String[] args) 
+	{
+		int ar[] = {1,3,5,2,4};
+		String s = Arrays.toString(ar);
+		System.out.println(s);
+	}
+}
