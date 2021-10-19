ef9d0a60d06c7f81012b685c1b36bf22df599920
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2021-10-19 22:57:07 +0530

    Create ConvertArrayToList.java

---
+++ ArrayList/ConvertArrayToList.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class ConvertArrayToList 
+{
+	public static void main(String[] args) 
+	{
+		Integer ar [] = {40,20,30,10};
+		List list = Arrays.asList(ar);
+		System.out.println(list);
+	}
+}
