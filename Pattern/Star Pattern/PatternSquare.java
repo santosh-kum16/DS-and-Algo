480575bcf636fa4c36a40b0bc7a74ff3d4c95dbe
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2021-10-25 22:47:29 +0530

    Create PatternSquare.java

---
+++ Pattern/Star Pattern/PatternSquare.java
@@ -0,0 +1,16 @@
+class PatternSquare
+{
+	public static void main(String[] args) 
+	{
+    // 5 row printing
+		int n=5;
+		for (int i=0;i<n ;i++)
+		{
+			for (int j=0;j<n ;j++ )
+			{
+				System.out.print("* ");	
+			}
+			System.out.println();
+		}
+	}
+}
