03b6a11d5a419f6a73aea5715d44a95bb7f71e6a
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2024-04-04 23:54:56 +0530

    Update AlternatingCharacters.java

--- HackerRank/AlternatingCharacters.java
+++ HackerRank/AlternatingCharacters.java
@@ -1,1 +1,33 @@
 // https://www.hackerrank.com/challenges/alternating-characters/problem
+
+import java.io.*;
+import java.util.*;
+
+public class AlternatingCharacters {
+
+    public static int consecutiveChars(String str)
+    {
+        StringBuilder mutableStr = new StringBuilder(str);
+        int numDeletions = 0;
+        int pos = 0;
+        while(pos<mutableStr.length()-1){
+            if(mutableStr.charAt(pos) == mutableStr.charAt(pos+1)){
+                mutableStr.deleteCharAt(pos);
+                numDeletions++;
+            }else{
+                pos++;
+            }
+        }
+        
+        
+        return numDeletions;
+    }
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int numCases = Integer.parseInt(scan.nextLine());
+        for(int i=0; i<numCases; i++){
+            System.out.println(consecutiveChars(scan.nextLine()));
+        }
+    }
+}
