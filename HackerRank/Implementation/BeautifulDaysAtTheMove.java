61409db7e106297b37c92e2ba043ad618b1f2e00
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2024-04-06 23:47:56 +0530

    Create BeautifulDaysAtTheMove.java

---
+++ HackerRank/Implementation/BeautifulDaysAtTheMove.java
@@ -0,0 +1,41 @@
+
+//https://www.hackerrank.com/challenges/beautiful-days-at-the-movies
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class BeautifulDaysAtTheMovies {
+
+    public static int reverseNum(int num)
+    {
+        String numStr = "" + num;
+        String reverseNumStr = "";
+        
+        for(int i = numStr.length() - 1;i >= 0;i--){
+            reverseNumStr = reverseNumStr + numStr.charAt(i);
+        }
+        
+        return Integer.parseInt(reverseNumStr);
+    }
+    
+    public static boolean isNumBeautiful(int num, int k)
+    {
+        return Math.abs(num - reverseNum(num)) % k == 0;
+    }
+    
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        int numOne = scan.nextInt();
+        int numTwo = scan.nextInt();
+        int k = scan.nextInt();
+        int total = 0;
+        for(int i = numOne; i <= numTwo; i++){
+            if(isNumBeautiful(i,k)) total++;
+        }
+        System.out.println(total);
+    }
+}
