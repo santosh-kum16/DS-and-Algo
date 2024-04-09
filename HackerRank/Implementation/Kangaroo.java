c2107d27758b9c06e5c7a828d58717045acefe1c
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2024-04-10 00:02:33 +0530

    Create Kangaroo.java

---
+++ HackerRank/Implementation/Kangaroo.java
@@ -0,0 +1,40 @@
+// https://www.hackerrank.com/challenges/kangaroo
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Kangaroo {
+
+    public static String willTheyMeat(int firstPos, int firstDistance, int secondPos, int secondDistance)
+    {
+        if (firstPos == secondPos) return "YES";
+        
+        while(firstPos < secondPos){
+            firstPos += firstDistance;
+            secondPos += secondDistance;
+            if(firstPos == secondPos) return "YES";   
+        }
+        
+        return "NO";
+    }
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x1 = scan.nextInt();
+        int v1 = scan.nextInt();
+        int x2 = scan.nextInt();
+        int v2 = scan.nextInt();
+        
+        if(x1 <= x2 && v1 >= v2){
+            System.out.println(willTheyMeat(x1,v1,x2,v2));
+        } else if (x2 <= x1 && v2 >= v1){
+            System.out.println(willTheyMeat(x2,v2,x1,v1));
+        } else {
+            System.out.println("NO");
+        }
+        
+    }
+}
